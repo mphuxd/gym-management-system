00615bc21ff6c0ff155baee48356b1ef7d5f85f1
Matthew <matthewpham2012@gmail.com>
2023-03-23 17:39:28 -0700

    refactor(Avatar): remove css module/update tokens

--- components/Avatar/Avatar.js
+++ components/Avatar/Avatar.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import * as AvatarPrimitive from '@radix-ui/react-avatar';
-import styles from './Avatar.module.scss';
 
 export const AVATAR_TEST_ID = 'avatarTestId';
 
 const Avatar = React.forwardRef(
   ({ children, id, src, alt, ...props }, forwardedRef) => (
     <AvatarPrimitive.Root
-      className={styles.AvatarRoot}
+      className="inline-flex h-12 w-12 select-none items-center justify-center overflow-hidden rounded-full align-middle"
       ref={forwardedRef}
       {...props}
       data-testid={AVATAR_TEST_ID}
     >
       <AvatarPrimitive.Image
-        className="rounded-full h-12 w-12"
+        className="h-12 w-12 rounded-full"
         src={src}
         alt={alt}
       />
