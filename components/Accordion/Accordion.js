4f755a30dcd777a4eecd002e024f22ef12cae8e2
Matthew <matthewpham2012@gmail.com>
2023-03-23 17:37:34 -0700

    refactor(accordion): remove css module & update tokens

--- components/Accordion/Accordion.js
+++ components/Accordion/Accordion.js
@@ -6,11 +6,11 @@ export const ACCORDION_TEST_ID = 'accordion';
 const Accordion = React.forwardRef(
   ({ type, defaultValue, collapsible, children, ...props }, forwardedRef) => (
     <AccordionPrimitive.Root
+      {...props}
       type={type}
       defaultValue={defaultValue}
       collapsible={collapsible}
       ref={forwardedRef}
-      {...props}
       data-testid={ACCORDION_TEST_ID}
     >
       {children}
