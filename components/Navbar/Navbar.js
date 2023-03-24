f670d9ec179b7d9134f0686977e6035465f1b27b
Matthew <matthewpham2012@gmail.com>
2023-03-23 17:46:44 -0700

    refactor(Navbar): update tokens/refactor

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ function Navbar({ children }) {
   return (
     <section id="navbar" className="col-span-full px-8">
       <nav>
-        <ul className="flex flex-row text-sm font-regular">{children}</ul>
+        <ul className="flex flex-row text-sm">{children}</ul>
       </nav>
     </section>
   );
--- components/Navbar/NavbarLink.js
+++ components/Navbar/NavbarLink.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import cx from 'classnames';
+import styles from './Navbar.module.scss';
 
 const NavbarLink = React.forwardRef(
   ({ children, className, href, ...props }, forwardedRef) => {
@@ -11,13 +12,11 @@ const NavbarLink = React.forwardRef(
     if (router.asPath === href) isActive = true;
     else if (router.asPath.includes(href) && href !== '/') isActive = true;
 
-    const classNames = cx(className, ' ', {
-      'px-4 relative text-base py-5': true, // default
-      'after:absolute after:w-1/2 after:translate-x-1/2 after:bg-red11 after:h-0.5 after:inset-0 after:inset-y-3/4 ':
-        isActive, // selected
-      'hover:after:absolute hover:after:w-1/2 after:translate-x-1/2 hover:after:bg-red7 hover:after:h-0.5 hover:after:inset-0 hover:after:inset-y-3/4':
-        !isActive, // not selected
-    });
+    const classNames = cx(
+      className,
+      'px-4 relative text-base py-5',
+      isActive ? styles.navLinkActive : styles.navLinkInactive
+    );
 
     return (
       <Link ref={forwardedRef} href={href} {...props} className={classNames}>
