f2a778f8e4fdc42b3ff8386f4fc8e7a333ac4ad9
Matthew <matthewpham2012@gmail.com>
2023-04-09 22:49:59 -0700

    feat(storybook): add storybook
    - added storybook
    - added fontsource for better compatibility with storybook
    - updated gitignore

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import React from 'react';
-// eslint-disable-next-line camelcase
-import { IBM_Plex_Sans } from '@next/font/google';
 import { UserProvider } from '@auth0/nextjs-auth0';
 import { ApolloProvider } from '@apollo/client';
 import { Provider as JotaiProvider } from 'jotai';
 import { Provider as ToastProvider } from '@radix-ui/react-toast';
 import apolloClient from '@/lib/apollo.ts';
 import { Layout } from '@/components';
+import '@fontsource/ibm-plex-sans/latin.css';
 import '../styles/styles.css';
 
 // @@ Enable Axe-core accessibility testing
@@ -19,31 +18,19 @@ import '../styles/styles.css';
 // @@ Enable stack trace for mem leaks
 // process.on("warning", (e) => console.warn(e.stack));
 
-const IBMPlexSans = IBM_Plex_Sans({
-  subsets: 'latin',
-  weight: ['100', '200', '300', '400', '500', '600', '700'],
-});
-
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <style jsx global>{`
-        html {
-          font-family: ${IBMPlexSans.style.fontFamily};
-        }
-      `}</style>
-      <UserProvider>
-        <ApolloProvider client={apolloClient}>
-          <JotaiProvider>
-            <ToastProvider>
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-            </ToastProvider>
-          </JotaiProvider>
-        </ApolloProvider>
-      </UserProvider>
-    </>
+    <UserProvider>
+      <ApolloProvider client={apolloClient}>
+        <JotaiProvider>
+          <ToastProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </ToastProvider>
+        </JotaiProvider>
+      </ApolloProvider>
+    </UserProvider>
   );
 }
 
